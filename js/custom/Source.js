fae21a1d8155bb90d32f7ca3b2403992897b57a7
P. Jagielski <jagielski.przemek@yahoo.com>
2017-10-23 13:50:19 -0400

    Added a simple 'jitter' animation

--- js/custom/Source.js
+++ js/custom/Source.js
@@ -57,44 +57,67 @@ var Tree = function (vec1, vec2) {
     this.vec2 = vec2;
     this.isDrawn = false;
 
+    this.jitter = function () {
+        this.vec1.x += 2 * Math.random() - 1;
+        this.vec1.y += 2 * Math.random() - 1;
+    };
+
     this.draw = function (ctx) {
         ctx.fillStyle = 'black';
         ctx.beginPath();
         ctx.moveTo(vec1.x, vec1.y);
         ctx.lineTo(vec2.x, vec2.y);
         ctx.stroke();
-    }
+    };
 
     this.getLeftBranch = function () {
         var modifiedVec = this.vec1.copy().sub(this.vec2),
             branch = modifiedVec.copy().rotate(180 + 25);
 
         return new Tree(vec2, vec2.copy().add(branch.div(new Vector(1.6, 1.4))));
-    }
+    };
 
     this.getRightBranch = function () {
         var modifiedVec = this.vec1.copy().sub(this.vec2),
             branch = modifiedVec.copy().rotate(180 - 25);
 
         return new Tree(vec2, vec2.copy().add(branch.div(new Vector(1.6, 1.4))));;
-    }
+    };
 };
 
 var canvas = document.getElementById("container"),
     ctx = canvas.getContext("2d");
 
 var myTree,
-    branches = [];
+    branches = [],
+    updateCounter;
 
 function init() {
-    myTree = new Tree(new Vector(canvas.width / 2, canvas.height), new Vector(canvas.width / 2, canvas.height - 100));
+    myTree = new Tree(new Vector(canvas.width / 2, canvas.height - 100), new Vector(canvas.width / 2, canvas.height - 200));
     branches.push(myTree);
+    this.updateCounter = 0;
+    for (var j = 0; j < 4; j++)
+        for (var i = branches.length - 1; i >= 0; i--) {
+            if (!branches[i].isDrawn) {
+                branches.push(branches[i].getLeftBranch());
+                branches.push(branches[i].getRightBranch());
+                branches[i].isDrawn = true;
+            }
+        }
 }
 
 function update() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    if (branches.length > 0) branches.forEach(function (i) { i.draw(ctx); });
+    if (branches.length > 0) branches.forEach(function (i) {
+        i.draw(ctx);
+    });
+
+    if (branches.length > 0) branches.forEach(function (i) {
+        i.jitter();
+    });
+
+    updateCounter++;
 }
 
 function onKeyPress(e) {}
@@ -105,17 +128,17 @@ function onMouseClick() {
     //    var tempTree = new Tree(myTree.vec2, tempArr[i]);
     //    branches.push(tempTree);
     //}
-    for (var i = branches.length - 1; i >= 0; i--)
-    {
-        if (!branches[i].isDrawn)
-        {
-            branches.push(branches[i].getLeftBranch());
-            branches.push(branches[i].getRightBranch());
-            branches[i].isDrawn = true;
-        }
-    }
+    //for (var i = branches.length - 1; i >= 0; i--)
+    //{
+    //    if (!branches[i].isDrawn)
+    //    {
+    //        branches.push(branches[i].getLeftBranch());
+    //        branches.push(branches[i].getRightBranch());
+    //        branches[i].isDrawn = true;
+    //    }
+    //}
 
-    console.log(branches);
+    //console.log(branches);
 }
 
 function main() {
@@ -123,5 +146,5 @@ function main() {
     canvas.height = 760;
 
     init();
-    setInterval(update, 6 / 100);
+    setInterval(update, 30 / 1000);
 } main();
